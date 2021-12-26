a7ba382366677f29ef5b1ff3d87fcfc67b190abf
clementlefevre <clement.lefevre@deutschebahn.com>
2021-12-26 19:08:59 +0100

    page_98

--- src/cljs/guestbook/core.cljs
+++ src/cljs/guestbook/core.cljs
@@ -1,36 +1,84 @@
 (ns guestbook.core (:require [reagent.core :as r]
                              [reagent.dom
                               :as dom]
+                             [re-frame.core :as rf]
                              [ajax.core :refer [GET POST]]
-                             [clojure.string :as string]))
+                             [clojure.string :as string]
+                             [guestbook.validation :refer [validate-message]]))
 
+
+(rf/reg-event-fx
+ :app/initialize
+ (fn [_ _]
+   {:db {:messages/loading? true}}))
+
+(rf/reg-event-db
+ :message/add
+ (fn [db [_ message]]
+   (update db :messages/list conj message)))
+
+(rf/reg-event-db
+ :messages/set
+ (fn [db [_ messages]]
+   (-> db
+       (assoc :messages/loading? false
+              :messages/list messages))))
+
+
+
+(rf/reg-sub
+ :messages/loading?
+ (fn [db _]
+   (:messages/loading? db)))
+
+
+(rf/reg-sub
+ :messages/list
+ (fn [db _]
+   (:messages/list db [])))
+
+(defn get-messages []
+  (GET "/messages"
+    {:headers {"Accept" "application/transit+json"}
+     :handler #(rf/dispatch [:messages/set (:messages %)])}))
 
 (defn send-message! [fields errors]
-  (POST "/message"
-    {:format :json
-     :headers
-     {"Accept" "application/transit+json"
-      "x-csrf-token" (.-value (.getElementById js/document "token"))}
-     :params @fields
-     :handler (fn [r]
-                (.log js/console (str "response:" r))
-                (reset! errors nil))
-     :error-handler (fn [e]
-                      (.log js/console (str e))
-                      (reset! errors (-> e :response :errors)))}))
-
+  (if-let [validation-errors (validate-message @fields)]
+    (reset! errors validation-errors)
+    (POST "/message"
+      {:format :json
+       :headers
+       {"Accept" "application/transit+json"
+        "x-csrf-token" (.-value (.getElementById js/document "token"))}
+       :params @fields
+       :handler (fn [_]
+                  (rf/dispatch
+                   [:message/add (assoc @fields :timestamp (js/Date.))])
+                  (reset! fields nil)
+                  (reset! errors nil))
+       :error-handler (fn [e]
+                        (.log js/console (str e))
+                        (reset! errors (-> e :response :errors)))})))
 
 (defn errors-component [errors id]
   (when-let [error (id @errors)]
     [:div.notification.is-danger (string/join error)]))
+
+(defn message-list [messages]
+  (println messages)
+  [:ul.messages
+   (for [{:keys [timestamp message name]} @messages]
+     ^{:key timestamp}
+     [:li
+      [:time (.toLocaleString timestamp)]
+      [:p message]
+      [:p " - " name]])])
 
 (defn message-form []
   (let [fields (r/atom {})
         errors (r/atom nil)]
     (fn []
       [:div
-       [:p "Name: " (:name @fields)]
-       [:p "Message: " (:message @fields)]
        [errors-component errors :server-error]
        [:div.field
         [:label.label {:for :name} "Name"]
@@ -55,10 +103,24 @@
          :value "comment"}]])))
 
 
+
+
+
 (defn home []
-  [:div.content>div.columns.is-centered>div.column.is-two-thirds
-   [:div.columns>div.column
-    [message-form]]])
+  (let [messages (rf/subscribe [:messages/list])]
+    (rf/dispatch [:app/initialize])
+    (get-messages)
+    (fn []
+      [:div.content>div.columns.is-centered>div.column.is-two-thirds
+       (if @(rf/subscribe [:messages/loading?])
+         [:h3 "Loading Messages..."]
+         [:div
+          [:div.columns>div.column
+           [:h3 "Messages"]
+           [message-list messages]]
+          [:div.columns>div.column
+           [message-form]]])])))
+
 
 
 (dom/render
