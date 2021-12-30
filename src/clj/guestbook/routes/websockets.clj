52ea338bd1d7563167749783d66746ba6e58aa47
clementlefevre <clement.lefevre@deutschebahn.com>
2021-12-30 14:11:02 +0100

    page_157

--- src/clj/guestbook/routes/websockets.clj
+++ src/clj/guestbook/routes/websockets.clj
@@ -1,48 +1,87 @@
+;---
+; Excerpted from "Web Development with Clojure, Third Edition",
+; published by The Pragmatic Bookshelf.
+; Copyrights apply to this code. It may not be used to create training material,
+; courses, books, articles, and the like. Contact us if you are in doubt.
+; We make no guarantees that this code is fit for any purpose.
+; Visit http://www.pragmaticprogrammer.com/titles/dswdcloj3 for more book information.
+;---
+;
 (ns guestbook.routes.websockets
-  (:require [clojure.tools.logging :as log]
-            [org.httpkit.server :as http-kit]
-            [clojure.edn :as edn]
-            [guestbook.messages :as msg]))
+  (:require
+   [clojure.tools.logging :as log]
+   [guestbook.messages :as msg]
+   [guestbook.middleware :as middleware]
+   [mount.core :refer [defstate]]
+   [taoensso.sente :as sente]
+   [taoensso.sente.server-adapters.http-kit :refer [get-sch-adapter]]))
+;
 
+;
+(defstate socket
+  :start (sente/make-channel-socket!
+          (get-sch-adapter)
+          {:user-id-fn (fn [ring-req]
+                         (get-in ring-req [:params :client-id]))}))
 
-(defonce channels (atom #{}))
+(defn send! [uid message]
+  (println "Sending message: " message)
+  ((:send-fn socket) uid message))
+;
 
-(defn connect! [channel]
-  (log/info "Channel opened")
-  (swap! channels conj channel))
+;
+(defmulti handle-message (fn [{:keys [id]}]
+                           id))
 
-(defn disconnect! [channel status]
-  (log/info "Channel closed: " status)
-  (swap! channels disj channel))
+(defmethod handle-message :default
+  [{:keys [id]}]
+  (log/debug "Received unrecognized websocket event type: " id)
+  {:error (str "Unrecognized websocket event type: " (pr-str id))
+   :id    id})
 
-
-(defn handle-message! [channel ws-message]
-  (let [message (edn/read-string ws-message)
-        response (try
-                   (msg/save-message! message)
-                   (assoc message :timestamp (java.util.Date.))
+(defmethod handle-message :message/create!
+  [{:keys [?data uid] :as message}]
+  (let [response (try
+                   (msg/save-message! ?data)
+                   (assoc ?data :timestamp (java.util.Date.))
                    (catch Exception e
-                     (let [{id
-                            :guestbook/error-id
+                     (let [{id     :guestbook/error-id
                             errors :errors} (ex-data e)]
                        (case id
                          :validation
                          {:errors errors}
-;;else
+                         ;;else
                          {:errors
                           {:server-error ["Failed to save message!"]}}))))]
     (if (:errors response)
-      (http-kit/send! channel (pr-str response))
-      (doseq [channel @channels]
-        (http-kit/send! channel (pr-str response))))))
+      (do
+        (log/debug "Failed to save message: " ?data)
+        response)
+      (do
+        (doseq [uid (:any @(:connected-uids socket))]
+          (send! uid [:message/add response]))
+        {:success true}))))
 
+(defn receive-message! [{:keys [id ?reply-fn]
+                         :as   message}]
+  (log/debug "Got message with id: " id)
+  (let [reply-fn (or ?reply-fn (fn [_]))]
+    (when-some [response (handle-message message)]
+      (reply-fn response))))
+;
 
-(defn handler [request]
-  (http-kit/with-channel request channel
-    (connect! channel)
-    (http-kit/on-close channel (partial disconnect! channel))
-    (http-kit/on-receive channel (partial handle-message! channel))))
+;
+(defstate channel-router
+  :start (sente/start-chsk-router!
+          (:ch-recv socket)
+          #'receive-message!)
+  :stop (when-let [stop-fn channel-router]
+          (stop-fn)))
 
 (defn websocket-routes []
   ["/ws"
-   {:get handler}])
+   {:middleware [middleware/wrap-csrf
+                 middleware/wrap-formats]
+    :get (:ajax-get-or-ws-handshake-fn socket)
+    :post (:ajax-post-fn socket)}])
+;
--- src/cljs/guestbook/core.cljs
+++ src/cljs/guestbook/core.cljs
@@ -1,12 +1,26 @@
-(ns guestbook.core (:require [reagent.core :as r]
-                             [reagent.dom
-                              :as dom]
-                             [re-frame.core :as rf]
-                             [ajax.core :refer [GET POST]]
-                             [clojure.string :as string]
-                             [guestbook.validation :refer [validate-message]]
-                             [guestbook.websockets :as ws]))
-
+;---
+; Excerpted from "Web Development with Clojure, Third Edition",
+; published by The Pragmatic Bookshelf.
+; Copyrights apply to this code. It may not be used to create training material,
+; courses, books, articles, and the like. Contact us if you are in doubt.
+; We make no guarantees that this code is fit for any purpose.
+; Visit http://www.pragmaticprogrammer.com/titles/dswdcloj3 for more book information.
+;---
+;
+(ns guestbook.core
+  (:require
+   ;
+   [reagent.core :as r]
+   [reagent.dom :as dom]
+   [re-frame.core :as rf]
+   [ajax.core :refer [GET POST]]
+   [clojure.string :as string]
+   [guestbook.validation :refer [validate-message]]
+   [guestbook.websockets :as ws]
+   ;
+   ;;...
+   [mount.core :as mount]))
+;
 
 (rf/reg-event-fx
  :app/initialize
@@ -14,19 +28,31 @@
    {:db {:messages/loading? true}
     :dispatch [:messages/load]}))
 
+;
+(rf/reg-fx
+ :ajax/get
+ (fn [{:keys [url success-event error-event success-path]}]
+   (GET url
+     (cond-> {:headers {"Accept" "application/transit+json"}}
+       success-event (assoc :handler
+                            #(rf/dispatch
+                              (conj success-event
+                                    (if success-path
+                                      (get-in % success-path)
+                                      %))))
+       error-event   (assoc :error-handler
+                            #(rf/dispatch
+                              (conj error-event %)))))))
+
 (rf/reg-event-fx
  :messages/load
  (fn [{:keys [db]} _]
-   (GET "/api/messages"
-     {:headers {"Accept" "application/transit+json"}
-      :handler #(rf/dispatch [:messages/set (:messages %)])})
-   {:db (assoc db :messages/loading? true)}))
+   {:db (assoc db :messages/loading? true)
+    :ajax/get {:url "/api/messages"
+               :success-path [:messages]
+               :success-event [:messages/set]}}))
+;
 
-
-(rf/reg-event-db
- :message/add
- (fn [db [_ message]]
-   (update db :messages/list conj message)))
 
 (rf/reg-event-db
  :messages/set
@@ -40,11 +66,25 @@
  (fn [db _]
    (:messages/loading? db)))
 
-
 (rf/reg-sub
  :messages/list
  (fn [db _]
    (:messages/list db [])))
+
+(defn message-list [messages]
+  [:ul.messages
+   (for [{:keys [timestamp message name]} @messages]
+     ^{:key timestamp}
+     [:li
+      [:time (.toLocaleString timestamp)]
+      [:p message]
+      [:p " - " name]])])
+
+(rf/reg-event-db
+ :message/add
+ (fn [db [_ message]]
+   (update db :messages/list conj message)))
+
 
 (rf/reg-event-db
  :form/set-field
@@ -62,12 +102,12 @@
  :form/fields
  (fn [db _]
    (:form/fields db)))
+
 (rf/reg-sub
  :form/field
  :<- [:form/fields]
  (fn [fields [_ id]]
    (get fields id)))
-
 
 (rf/reg-event-db
  :form/set-server-errors
@@ -80,8 +120,7 @@
  (fn [db _]
    (:form/server-errors db)))
 
-
-  ;;Validation errors are reactively computed
+;;Validation errors are reactively computed
 (rf/reg-sub
  :form/validation-errors
  :<- [:form/fields]
@@ -107,74 +146,113 @@
  (fn [errors [_ id]]
    (get errors id)))
 
-
-
-
-(defn get-messages []
-  (GET  "/api/messages"
-    {:headers {"Accept" "application/transit+json"}
-     :handler #(rf/dispatch [:messages/set (:messages %)])}))
+;
+;;...
+;
+(rf/reg-event-fx
+ :message/send!-called-back
+ (fn [_ [_ {:keys [success errors]}]]
+   (if success
+     {:dispatch [:form/clear-fields]}
+     {:dispatch [:form/set-server-errors errors]})))
 
 (rf/reg-event-fx
  :message/send!
  (fn [{:keys [db]} [_ fields]]
-   (ws/send-message! fields)
-   {:db (dissoc db :form/server-errors)}))
-
-(defn handle-response! [response]
-  (if-let [errors (:errors response)]
-    (rf/dispatch [:form/set-server-errors errors])
-    (do
-      (rf/dispatch [:message/add response])
-      (rf/dispatch [:form/clear-fields response]))))
-
-
+   {:db (dissoc db :form/server-errors)
+    :ws/send! {:message [:message/create! fields]
+               :timeout 10000
+               :callback-event [:message/send!-called-back]}}))
+;
+;
 
 (defn errors-component [id]
   (when-let [error @(rf/subscribe [:form/error id])]
     [:div.notification.is-danger (string/join error)]))
 
-(defn message-list [messages]
-  (println messages)
-  [:ul.messages
-   (for [{:keys [timestamp message name]} @messages]
-     ^{:key timestamp}
-     [:li
-      [:time (.toLocaleString timestamp)]
-      [:p message]
-      [:p " - " name]])])
+;
+(defn text-input [{val   :value
+                   attrs :attrs
+                   :keys [on-save]}]
+  (let [draft (r/atom nil)
+        value (r/track #(or @draft @val ""))]
+    (fn []
+      [:input.input
+       (merge attrs
+              {:type :text
+               :on-focus #(reset! draft (or @val ""))
+               :on-blur (fn []
+                          (on-save (or @draft ""))
+                          (reset! draft nil))
+               :on-change #(reset! draft (.. % -target -value))
+               :value @value})])))
+;;...
 
+
+;
+
+;
+(defn textarea-input [{val   :value
+                       attrs :attrs
+                       :keys [on-save]}]
+  (let [draft (r/atom nil)
+        value (r/track #(or @draft @val ""))]
+    (fn []
+      [:textarea.textarea
+       (merge attrs
+              {:on-focus #(reset! draft (or @val ""))
+               :on-blur (fn []
+                          (on-save (or @draft ""))
+                          (reset! draft nil))
+               :on-change #(reset! draft (.. % -target -value))
+               :value @value})])))
+;;...
+
+
+;
+;
+;
 (defn message-form []
   [:div
+   ;;...
+   ;
+   ;
    [errors-component :server-error]
    [:div.field
     [:label.label {:for :name} "Name"]
     [errors-component :name]
-    [:input.input
-     {:type :text
-      :name :name
-      :on-change #(rf/dispatch
-                   [:form/set-field
-                    :name
-                    (.. % -target -value)])
-      :value @(rf/subscribe [:form/field :name])}]]
+    ;
+    [text-input {:attrs {:name :name}
+                 :value (rf/subscribe [:form/field :name])
+                 :on-save #(rf/dispatch [:form/set-field :name %])}]
+    ;;...
+    ;
+    ]
    [:div.field
     [:label.label {:for :message} "Message"]
     [errors-component :message]
-    [:textarea.textarea
-     {:name :message
-      :value @(rf/subscribe [:form/field :message])
-      :on-change #(rf/dispatch
-                   [:form/set-field
-                    :message
-                    (.. % -target -value)])}]]
+    ;
+    [textarea-input
+     {:attrs {:name :message}
+      :value (rf/subscribe [:form/field :message])
+      :on-save #(rf/dispatch [:form/set-field :message %])}]
+    ;;...
+    ;
+    ]
    [:input.button.is-primary
     {:type :submit
      :disabled @(rf/subscribe [:form/validation-errors?])
      :on-click #(rf/dispatch [:message/send!
                               @(rf/subscribe [:form/fields])])
-     :value "comment"}]])
+     :value "comment"}]
+;
+;
+   ])
+;
+;
 
+;
+;
 
 (defn reload-messages-button []
   (let [loading? (rf/subscribe [:messages/loading?])]
@@ -184,7 +262,6 @@
      (if @loading?
        "Loading Messages"
        "Refresh Messages")]))
-
 
 (defn home []
   (let [messages (rf/subscribe [:messages/list])]
@@ -204,15 +281,11 @@
   (dom/render [#'home] (.getElementById js/document "content"))
   (.log js/console "Components Mounted!"))
 
-
+;
+;;...
 (defn init! []
   (.log js/console "Initializing App...")
+  (mount/start)
   (rf/dispatch [:app/initialize])
-  (ws/connect! (str "ws://" (.-host js/location) "/ws")
-               handle-response!)
   (mount-components))
-
-
-(dom/render
- [home]
- (.getElementById js/document "content"))
+;
